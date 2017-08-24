f878e5e588636c3f5cea80725afcd440c7a4376e
Jürgen Smolka <30924797+ttimer@users.noreply.github.com>
2017-08-24 10:45:05 +0200

    Hello Tea World !
    Here is a-Tea-Timer-4-U.

---
+++ HelloTeaWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloTeaWorld {
+
+	/* @param args */
+	public static void main(String[] args) {
+		System.out.println("Hello Tea World !");
+		System.out.println("Here is a-Tea-Timer-4-U.");
+	}
+
+}
